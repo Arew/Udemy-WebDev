0d851d4ad58cd7388d044a58172e2d9c8524db87
Andrew Hurst <ah219908@gmail.com>
2017-03-04 16:54:00 -0800

    day 6a

---
+++ DOM Selectors/selector.js
@@ -0,0 +1,4 @@
+var first = document.getElementById("first");
+var second = document.getElementsByClassName("special")[0];
+var third = document.getElementsByTagName("p");
+var fourth = document.querySelector("#first");
